09b45b29bf427985cb0fedcd7d4d4ae54c8eff0e
skyegrey <dev.skyegrey@gmail.com>
2022-03-09 11:53:56 -0600

    Clicking no longer refresh whole page.

--- writing-your-first-angular-web-application/angular-reddit/src/app/article/article.component.ts
+++ writing-your-first-angular-web-application/angular-reddit/src/app/article/article.component.ts
@@ -17,12 +17,14 @@ export class ArticleComponent implements OnInit {
     this.votes = 10;
   }
 
-  voteUp() {
+  voteUp(): boolean {
     this.votes += 1;
+    return false;
   }
 
-  voteDown() {
+  voteDown(): boolean {
     this.votes -= 1;
+    return false;
   }
 
   ngOnInit(): void {
