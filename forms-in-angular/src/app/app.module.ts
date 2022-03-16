de92462e21e2a2f90eef0e68efef355a876872b7
skyegrey <dev.skyegrey@gmail.com>
2022-03-16 11:26:28 -0500

    Setup new project for forms demo

---
+++ forms-in-angular/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms'
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoFormSkuComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
