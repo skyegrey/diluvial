a0980737bcb87d12645d1a5740f2def02ae8e9cc
skyegrey <dev.skyegrey@gmail.com>
2022-03-25 11:37:24 -0500

    Added subscriptions to log input changes

--- forms-in-angular/src/app/demo-form-sku/demo-form-sku.component.ts
+++ forms-in-angular/src/app/demo-form-sku/demo-form-sku.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {
   AbstractControl,
-  FormBuilder,
+  FormBuilder, FormControl,
   FormGroup,
   Validators
 } from "@angular/forms";
+
+
+function skuValidator(control: FormControl): { [s: string]: boolean } | null {
+  if (!control.value.match(/^123/)) {
+    return {invalidSku: true}
+  }
+
+  return null;
+}
 
 @Component({
   selector: 'app-demo-form-sku',
@@ -17,10 +26,24 @@ export class DemoFormSkuComponent implements OnInit {
 
   constructor(fb: FormBuilder) {
     this.myForm = fb.group({
-      'sku': ['', Validators.required],
+      'sku': ['', Validators.compose([
+        Validators.required,
+        skuValidator])],
     })
 
     this.sku = this.myForm.controls['sku'];
+
+    this.sku.valueChanges.subscribe(
+      (value: string) => {
+        console.log('sku changed to: ', value);
+      }
+    )
+
+    this.myForm.valueChanges.subscribe(
+      (form: any) => {
+        console.log('form changed to: ', form);
+      }
+    )
   }
 
   ngOnInit(): void {
